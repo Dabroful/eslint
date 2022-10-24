df863e8729a4c06ad9f7ddaa7b25b94da4305f40
Vitaly Kovalev <kovalevvitalii@mail.ru>
2022-10-24 19:39:58 +0300

    e
    e

---
+++ cart.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+  getTotalPrice() {
+    return this.totalPrice;
+  },
+
+  add(name, price, count = 1) {
+    const obj = {name, price, count};
+    cart.items.push(obj);
+    this.increaseCount(count);
+    this.calculateItemPrice(price, count);
+  },
+  increaseCount(count) {
+    this.count += count;
+  },
+
+  calculateItemPrice(price, count) {
+    return this.totalPrice += price * count;
+  },
+
+  clear() {
+    this.totalPrice = 0;
+    this.count = 0;
+    this.items = [];
+  },
+  print() {
+    console.log(JSON.stringify(cart));
+  },
+};
+
+cart.add('notebook', 132000, 1);
+cart.add('mouse', 1500, 2);
+cart.add('pads', 5500, 4);
+cart.print();
